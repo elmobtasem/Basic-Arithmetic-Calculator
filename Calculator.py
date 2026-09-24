93c4d44aae91f0450d603bd833b32562e96aa0f3
Ibrahim Amer <adhk6253@gmail.com>
2026-09-24 13:26:03 +0300

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,12 @@
+print("|Welcome to Calculator App|\n")
+print("*****BY:Ibrahim Mohamed*****\n")
+num1= float(input("First Number : "))
+num2= float(input("Second Number : "))
+
+print("******************************")
+
+print("Adding + : "+str(num1+num2))
+print("Subtract - : "+str(num1-num2))
+print("Multiply * : "+str(num1*num2))
+print("Divide / : "+str(num1/num2))
+print("Modulus % : "+str(num1%num2))
